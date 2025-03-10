90d7fd26b42ef12d613e52faedc7a8937df1eae1
mestreltsov <mestreltsov@edu.hse.ru>
2025-03-10 23:06:09 +0300

    handle secrets

--- src/main/java/ru/hse/mmstr_project/se/EmergencyNotificationApplication.java
+++ src/main/java/ru/hse/mmstr_project/se/EmergencyNotificationApplication.java
@@ -1,13 +1,27 @@
 package ru.hse.mmstr_project.se;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableEncryptableProperties
 public class EmergencyNotificationApplication {
+	@Value("${kafka.username}")
+	private String kafkaUsername;
 
-	public static void main(String[] args) {
+	@Value("${kafka.password}")
+	private String kafkaPassword;
+
+    public static void main(String[] args) {
 		SpringApplication.run(EmergencyNotificationApplication.class, args);
 	}
 
+	@PostConstruct
+	public void init() {
+		System.out.println("Kafka Username: " + kafkaUsername);
+		System.out.println("Kafka Password: " + kafkaPassword);
+	}
 }
---
+++ src/main/java/ru/hse/mmstr_project/se/utils/secrets/SecretManualEncryptorConfig.java
@@ -0,0 +1,22 @@
+package ru.hse.mmstr_project.se.utils.secrets;
+
+
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SecretManualEncryptorConfig {
+
+    @Bean("jasyptStringEncryptor")
+    public StringEncryptor stringEncryptor(
+            @Value("${jasypt.encryptor.password}") String password,
+            @Value("${jasypt.encryptor.algorithm}") String algorithm) {
+        StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
+        encryptor.setPassword(password);
+        encryptor.setAlgorithm(algorithm);
+        return encryptor;
+    }
+}
