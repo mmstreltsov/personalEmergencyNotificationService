8005215b02225950b2c87963f3b09d03e4b32a75
mestreltsov <mestreltsov@edu.hse.ru>
2025-03-10 23:09:57 +0300

    .

--- src/main/java/ru/hse/mmstr_project/se/utils/secrets/encode.sh
+++ src/main/java/ru/hse/mmstr_project/se/utils/secrets/encode.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-~/IdeaProjects/jasypt-1.9.3/bin/encrypt.sh input=data password=$JASYPT_MY_MAIN_SECRET_IS algorithm=PBEWithMD5AndTripleDES
+~/IdeaProjects/jasypt-1.9.3/bin/encrypt.sh input=data password="$JASYPT_MY_MAIN_SECRET_IS" algorithm=PBEWithMD5AndTripleDES
