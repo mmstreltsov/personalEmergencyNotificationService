e72a52771e0134dd7109679f3bd72cdb6e845379
mestreltsov <mestreltsov@edu.hse.ru>
2025-03-10 22:00:15 +0300

    init

---
+++ src/main/java/ru/hse/mmstr_project/se/EmergencyNotificationApplication.java
@@ -0,0 +1,13 @@
+package ru.hse.mmstr_project.se;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmergencyNotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmergencyNotificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/hse/mmstr_project/se/EmergencyNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.hse.mmstr_project.se;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmergencyNotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
