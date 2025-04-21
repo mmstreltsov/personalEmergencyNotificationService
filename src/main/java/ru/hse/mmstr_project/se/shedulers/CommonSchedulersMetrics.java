5ea9b0b8a19a3e6d5229075d13a6bdd501c7f2d2
mestreltsov <mestreltsov@edu.hse.ru>
2025-04-21 23:44:00 +0300

    .

--- src/main/java/ru/hse/mmstr_project/se/shedulers/CommonSchedulersMetrics.java
+++ src/main/java/ru/hse/mmstr_project/se/shedulers/CommonSchedulersMetrics.java
@@ -3,6 +3,7 @@ package ru.hse.mmstr_project.se.shedulers;
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.Gauge;
 import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Timer;
 import org.springframework.stereotype.Component;
 
 import java.util.concurrent.atomic.AtomicLong;
@@ -12,15 +13,21 @@ public class CommonSchedulersMetrics {
 
     private final Counter counter;
     private final AtomicLong timeWindowValueSec = new AtomicLong(0L);
+    private final Timer requestTimer;
 
     public CommonSchedulersMetrics(MeterRegistry meterRegistry) {
         this.counter = Counter.builder("scheduler.common.processing.count")
                 .description("Total amount of processed items")
                 .tag("place", "schedulers")
                 .register(meterRegistry);
-        Gauge.builder("scheduler.common.processing.time", timeWindowValueSec, AtomicLong::get)
+        Gauge.builder("scheduler.common.window.value", timeWindowValueSec, AtomicLong::get)
                 .description("Time window for db request")
                 .tag("place", "schedulers")
+                .register(meterRegistry);
+        this.requestTimer = Timer.builder("scheduler.common.batch.processing.time")
+                .description("Batch processing time")
+                .tag("place", "schedulers")
+                .publishPercentileHistogram() // Это включает гистограмму в Prometheus
                 .register(meterRegistry);
     }
 
@@ -30,5 +37,9 @@ public class CommonSchedulersMetrics {
 
     public void setTimeWindowValueSec(long timeDelta) {
         timeWindowValueSec.set(timeDelta);
+    }
+
+    public void measureRequest(Runnable action) {
+        requestTimer.record(action);
     }
 }
