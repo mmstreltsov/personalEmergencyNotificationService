f915e8ed7f8ddb406764f45d9690eff564526c86
mestreltsov <mestreltsov@edu.hse.ru>
2025-04-26 21:26:55 +0300

    improve executors

--- src/main/java/ru/hse/mmstr_project/se/shedulers/SchedulersContext.java
+++ src/main/java/ru/hse/mmstr_project/se/shedulers/SchedulersContext.java
@@ -3,6 +3,7 @@ package ru.hse.mmstr_project.se.shedulers;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import ru.hse.mmstr_project.se.shedulers.metrics.ExecutorMetrics;
 
 import java.util.concurrent.Executor;
 
@@ -10,19 +11,44 @@ import java.util.concurrent.Executor;
 public class SchedulersContext {
 
     @Bean(name = "taskExecutorForCommonStorage")
-    public Executor taskExecutorForCommonStorage() {
+    public Executor taskExecutorForCommonStorage(ExecutorMetrics metrics) {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+
         executor.setCorePoolSize(5);
+
         executor.setThreadNamePrefix("common-data-processor-");
+
+        executor.setTaskDecorator(task -> () -> {
+            try {
+                task.run();
+            } catch (Exception e) {
+                metrics.incCommonExecutorError();
+                throw e;
+            }
+        });
+
         executor.initialize();
         return executor;
     }
 
     @Bean(name = "taskExecutorForFastStorage")
-    public Executor taskExecutorForFastStorage() {
+    public Executor taskExecutorForFastStorage(ExecutorMetrics metrics) {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+
         executor.setCorePoolSize(5);
+
         executor.setThreadNamePrefix("fast-data-processor-");
+        executor.setThreadPriority(Thread.MAX_PRIORITY - 1);
+
+        executor.setTaskDecorator(task -> () -> {
+            try {
+                task.run();
+            } catch (Exception e) {
+                metrics.incFastExecutorError();
+                throw e;
+            }
+        });
+
         executor.initialize();
         return executor;
     }
