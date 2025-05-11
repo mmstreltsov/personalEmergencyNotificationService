38a076f209b7a47f92d1358e6b6e696bb73682a6
mestreltsov <mestreltsov@edu.hse.ru>
2025-05-11 13:04:52 +0300

    fix

--- src/main/java/ru/hse/mmstr_project/se/service/sender/SenderService.java
+++ src/main/java/ru/hse/mmstr_project/se/service/sender/SenderService.java
@@ -3,6 +3,7 @@ package ru.hse.mmstr_project.se.service.sender;
 import org.springframework.stereotype.Component;
 import ru.hse.mmstr_project.se.kafka.dto.SenderRequestDto;
 import ru.hse.mmstr_project.se.service.kafka.producer.SenderProducer;
+import ru.hse.mmstr_project.se.storage.fast_storage.dto.FriendMetaDto;
 import ru.hse.mmstr_project.se.storage.fast_storage.dto.IncidentMetadataDto;
 
 import java.util.List;
@@ -18,15 +19,20 @@ public class SenderService {
 
     public void send(List<IncidentMetadataDto> requests) {
         requests.stream()
-                .flatMap(request -> request.listOfFriends().stream().map(friend -> new SenderRequestDto(
-                        request.text(),
-                        null,
-                        request.username(),
-                        request.telegramId(),
-                        friend.wayToNotify(),
-                        friend.phoneNumber(),
-                        friend.chatId(),
-                        friend.email())))
+                .flatMap(request -> request.listOfFriends().stream()
+                        .filter(friend -> request.listOfFriends().stream()
+                                .map(FriendMetaDto::id)
+                                .toList()
+                                .contains(friend.id()))
+                        .map(friend -> new SenderRequestDto(
+                                request.text(),
+                                null,
+                                request.username(),
+                                request.telegramId(),
+                                friend.wayToNotify(),
+                                friend.phoneNumber(),
+                                friend.chatId(),
+                                friend.email())))
                 .forEach(this::sendOne);
     }
 
